b67647d83735b1ad8dc5a3921a706fd102d7a249
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 09:46:53 -0400

    odd number counter

---
+++ count_odds.py
@@ -0,0 +1,19 @@
+# Add the odd numbers between 0 and 5000
+
+# setup
+result = 0
+
+# work1
+# let's look at the numbers form 0 to 5000
+num = 0
+while num <= 5000:
+    print(num)
+    num += 1
+
+
+
+
+
+
+
+print(result)
